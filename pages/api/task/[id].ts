797d4c44abca76bcc3d25ffd4c8be2cfe2c240d7
David Fernando Briceño Galindo <61285906+Dafbrig@users.noreply.github.com>
2022-02-11 18:48:36 -0500

    Update

--- pages/api/task/[id].ts
+++ pages/api/task/[id].ts
@@ -2,7 +2,7 @@ import {NextApiRequest,NextApiResponse}from "next";
 import { conexion } from "../../utiles/database";
 
 export default async (req: NextApiRequest, res:NextApiResponse) => {
-    const {method, query}=req;
+    const {method, query,body}=req;
     console.log(query);
     switch (method) {
         case "GET":
@@ -17,7 +17,17 @@ export default async (req: NextApiRequest, res:NextApiResponse) => {
                 return res.status(500).json({message: error.message});
             }
         case "Put":
-
+            try {
+                const {Num_Pisos, Area_Total, Direccion}= body;
+                const text="Update Construccion set Num_Pisos=$1, Area_Total=$2, Direccion=$3  where id=$4 returning *";
+                const values = [Num_Pisos, Area_Total, Direccion, query.id];
+                const result =await conexion.query(text, values);
+                if (result.rows.length == 0)
+                    return res.status(404).json({message: "No encontrado"});
+                    return res.json(result.rows[0]);
+            } catch (error: any) {
+                return res.status(500).json({message: error.message});
+            }
         case "Delete":
             try {
                 const text="Delete from Construccion where id=$1 returning *";
