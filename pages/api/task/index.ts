f57398818bbc874647af659f06ac7644c2655b51
David Fernando Briceño Galindo <61285906+Dafbrig@users.noreply.github.com>
2022-02-11 16:52:05 -0500

    organizamiento

--- pages/api/task/index.ts
+++ pages/api/task/index.ts
@@ -6,13 +6,24 @@ export default async (req:NextApiRequest, res:NextApiResponse) =>{
 
     switch(method){
         case 'GET':
+            try{
+            const query = "Select * From Construccion"
+            const response = await conexion.query(query)
+            console.log(response)
+
             return res.json("getting tasks");
-        case 'POST':
+        }catch(error){
+            console.log(error)
+        }
+        case 'POST':try {
             const {Cod_Cons, Num_Pisos, Area_Total,Direccion}=body;
             const query = "insert into Construccion(Cod_Cons, Num_Pisos, Area_Total,Direccion) values ($1,$2,$3,$4) returning *"
             const values =[Cod_Cons, Num_Pisos, Area_Total, Direccion]
             const response = await conexion.query(query,values)
             return res.json(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }
         case 'DELETE':
             return res.json("deleting task");
         break;
