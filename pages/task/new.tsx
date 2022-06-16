6ba96fcdd60e4fea7ac34059031aa85c5302b7d1
David Fernando Briceño Galindo <61285906+Dafbrig@users.noreply.github.com>
2022-06-16 18:47:23 -0500

    Editar y eliminar

--- pages/task/new.tsx
+++ pages/task/new.tsx
@@ -1,4 +1,4 @@
-import {Card, Form, Button} from 'semantic-ui-react'
+import {Card, Form, Button, Grid, Confirm} from 'semantic-ui-react'
 import { ChangeEvent, FormEvent, useState, useEffect } from 'react'
 import { Task } from 'pages/interfaces/Task'
 import router, {useRouter} from 'next/router'
@@ -11,51 +11,98 @@ export default function newPage(){
         Area_Total:'',
         Direccion:''
     })
+
+    const [openConfirm, setopenConfirm] = useState(false)
+
     const router =useRouter();
     const handleChange = ({target: {name, value},}: ChangeEvent<HTMLInputElement|HTMLAreaElement>) => setTask ({...task,[name]:value})
 
     
     const createTask=async(task:Task)=>{ await fetch('http://localhost:3000/api/task',{method:'POST',headers:{'Content-Type':'aplication/json'},body: JSON.stringify(task)})}
 
+    const loadTask = async (id: string) => {
+        const res = await fetch("http://localhost:3000/api/tasks/"+id)
+        const task = await res.json()
+        setTask({Cod_Cons: task.Cod_Cons, Num_Pisos: task.Num_pisos, Area_Total: task.Area_Total, Direccion: task.Direccion  });
+    }
+
+    const updateTask = async (id:string, task: Task) => {
+        await fetch("http://localhost:3000/api/tasks"+id, {
+            method: "PUT",
+            headers :{
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify(task),
+        })
+    }
 
     const handleSumbit =async (e: FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
     try {
-        await createTask(task)
+        if (typeof router.query.id === 'string'){
+            updateTask(router.query.id, task)
+        }else{
+            await createTask(task)
+        }
         router.push("/")
     } catch (error) {
         console.log(error);
     }
 }
 
-useEffect(()=>{
-    if(router.query.id ===  )
+const handleDelete = async (id: string) => {
+    try{
+        await fetch("http://localhost:3000/api/tasks"+id, {method: "DELETE",});
+        router.push("/")
+    }catch(error){
+        console.log(error)
+    }
+}
 
+useEffect(()=>{
+    if(typeof router.query.id === 'string') loadTask(router.query.id);
 }, [router.query])
 
     return (
         <Layout>
-            <Card.Content>
-                <Form onSubmit={handleSumbit}>
-                    <Form.Field>
-                        <label htmlFor="Cod_Cons">Codigo de Construccion</label>
-                        <input type="number" placeholder="Escribir Codigo de construccion" name="cod_Cons" onChange={handleChange}/>
-                    </Form.Field>
-                    <Form.Field>
-                    <label htmlFor="Num_Pisos">Numero de Pisos</label>
-                    <input type="text" placeholder="Poner los pisos de la construccion" name="num_Pisos" onChange={handleChange}/>
-                    </Form.Field>
-                    <Form.Field>
-                    <label htmlFor="Area_Total">Area Total de la construccion</label>
-                    <input type="text" placeholder="Poner el Area Total de la construccion" name="area_Total" onChange={handleChange}/>
-                    </Form.Field>
-                    <Form.Field>
-                    <label htmlFor="Direccion">Direccion de la construccion</label>
-                    <input type="text" placeholder="Poner la direccion de la construccion" name="direccion" onChange={handleChange}/>
-                    </Form.Field>
-                    <button>Guardar</button>
-                </Form>
-            </Card.Content>
+            <Grid centered colums={3} verticalAlign='middle' style={{height: '70%'}}>
+                <Grid.Column>
+                    <Card>
+                        <Form onSubmit={handleSumbit}>
+                            <Form.Field>
+                                <label htmlFor="Cod_Cons">Codigo de Construccion</label>
+                                <input type="number" placeholder="Escribir Codigo de construccion" name="cod_Cons" onChange={handleChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                            <label htmlFor="Num_Pisos">Numero de Pisos</label>
+                            <input type="text" placeholder="Poner los pisos de la construccion" name="num_Pisos" onChange={handleChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                            <label htmlFor="Area_Total">Area Total Construccion</label>
+                            <input type="text" placeholder="Poner el Area Total de la construccion" name="area_Total" onChange={handleChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                            <label htmlFor="Direccion">Direccion construccion</label>
+                            <input type="text" placeholder="Poner la direccion de la construccion" name="direccion" onChange={handleChange}/>
+                            </Form.Field>
+                            {
+                                router.query.id ? (
+                                    <button color='Teal'>Actulizar</button>
+                                ):(
+                                    <button>Guardar</button>
+                                )
+                            }
+                        </Form>
+                    </Card>
+                    <Button color='red' onClick={() =>setopenConfirm(true)}>Eliminar</Button>
+                </Grid.Column>
+            </Grid>
+            <Confirm header='Eliminar Catastro' 
+            content='¿Seguro de la eliminacion del Catastro ${router.query.id}?'
+            open={openConfirm}
+            onCancel={()=>setopenConfirm(false)}
+            onConfirm={()=>typeof router.query.id === "string" && handleDelete(router.query.id)}
+            />
         </Layout>
     )
 }
